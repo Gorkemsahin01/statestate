3edf3372039a62a9c8696ce14445d3ab16e9f3b7
gorkemsahin <gorkemsahin012@gmail.com>
2024-02-20 15:45:04 +0300

    react new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Course from './Course'
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomCourse() {
+  const courseArray = ['HTML', 'CSS', 'JAVASCRİPT', 'REACT']
+  return courseArray[Math.floor(Math.random() * courseArray.length)]
 }
 
-export default App;
+function App() {
+  const [courses, setCourses] = useState([])
+
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()])
+    // ... daha öncekiler geçerli demek yani butona bastık CSS çıktı bidaha bastığımızda CSSin kaybolmaması için ...courses dememiz gerekli
+  }
+  const courseList = courses.map((course, index) => {
+    return <Course key={index} courseName={course} />
+  })
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Yazılım Dili Ekle</button>
+
+      <div>{courseList}</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Course.js
@@ -0,0 +1,14 @@
+import HTML from './images/html.png'
+import CSS from './images/CSS.png'
+import JAVASCRİPT from './images/javascript.png'
+import REACT from './images/react.png'
+
+function Course({ courseName }) {
+  return (
+    <div>
+      <img src="{HTML}" />
+    </div>
+  )
+}
+
+export default Course
